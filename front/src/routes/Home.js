712e5aeaf8dfa905bb3b76b36a2c741bb9761245
eastZoo <dongju010@naver.com>
2022-08-25 21:54:21 +0900

    ADD : 전체 html태그 스타일 세팅

--- front/src/routes/Home.js
+++ front/src/routes/Home.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import classes from './Home.module.css';
 
 const Home = () => {
+    const [newPost, setNewPost] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        console.log(newPost);
+        setNewPost("");
+    };
+
+    //input 입력 targeting
+    const onChange = (event) => {
+        const { target: { value },
+        } = event;
+        setNewPost(value);
+    };
+
     return (
-        <div>Home</div>
+        <div>
+            <form onSubmit={onSubmit} className={classes.factoryForm}>
+                <div className={classes.factoryInput__container}>
+                    <input
+                        className={classes.factoryInput__input}
+                        value={newPost}
+                        onChange={onChange}
+                        type="text"
+                        placeholder="Picker들과 공유할 Youtube URL을 넣어주세요!"
+                        maxLength={120}
+                    />
+                    <input type="submit" value="&rarr;" className={classes.factoryInput__arrow} />
+                </div>
+            </form>
+        </div>
     )
 }
 
