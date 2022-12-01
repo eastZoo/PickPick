81cfb77754d67db294b64a5d290f4b4ab4ee0640
jodongha <jodongrami@gmail.com>
2022-12-02 03:47:22 +0900

    UPDATE : Return Type

--- back/src/main/java/com/example/PickPick/controller/OAuthController.java
+++ back/src/main/java/com/example/PickPick/controller/OAuthController.java
@@ -1,14 +1,17 @@
 package com.example.PickPick.controller;
 
 import com.example.PickPick.config.security.JwtTokenProvider;
+import com.example.PickPick.dto.LoginResponseDto;
 import com.example.PickPick.dto.UserDto;
 import com.example.PickPick.service.OAuthService;
 import com.example.PickPick.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
-@RequestMapping("oauth")
+@RequestMapping("api")
 @RequiredArgsConstructor
 public class OAuthController {
 
@@ -16,12 +19,14 @@ public class OAuthController {
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    @GetMapping("/login/{code}")
-    public String login(@PathVariable String code) {
+    @PostMapping("/auth")
+    public LoginResponseDto login(@RequestBody String code) {
         String token = oAuthService.getKakaoAccessToken(code);
         UserDto user = oAuthService.getKakaoUserInfo(token);
         userService.userCheck(user);
 
-        return jwtTokenProvider.createToken(user);
+        return LoginResponseDto.builder()
+                .token(jwtTokenProvider.createToken(user))
+                .build();
     }
 }
