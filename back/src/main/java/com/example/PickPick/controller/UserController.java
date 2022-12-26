879c65fcca5e6b01ff434872651eedc9549d1e72
minseong-dev <aa971020@gmail.com>
2022-12-26 17:30:52 +0900

    REPACTOR : UserController 함수 반환타입

--- back/src/main/java/com/example/PickPick/controller/UserController.java
+++ back/src/main/java/com/example/PickPick/controller/UserController.java
@@ -2,11 +2,13 @@ package com.example.PickPick.controller;
 
 import com.example.PickPick.dto.ResultDto;
 import com.example.PickPick.dto.UserDto;
-import com.example.PickPick.dto.VideoDto;
+import com.example.PickPick.dto.VideoLikeDto;
 import com.example.PickPick.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
@@ -16,12 +18,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/videos")
-    public ResultDto<VideoDto> getSharedVideo(@RequestHeader("X-AUTH-TOKEN") String token) {
+    public ResultDto<UserDto.UserInfo> getSharedVideo(@RequestHeader("X-AUTH-TOKEN") String token) {
         return userService.getSharedVideo(token);
     }
 
     @GetMapping("/likeList")
-    public ResultDto getLikeList(@RequestHeader("X-AUTH-TOKEN") String token) {
+    public ResultDto<List<VideoLikeDto.VideoLikeList>> getLikeList(@RequestHeader("X-AUTH-TOKEN") String token) {
         return userService.getLikeList(token);
     }
 
