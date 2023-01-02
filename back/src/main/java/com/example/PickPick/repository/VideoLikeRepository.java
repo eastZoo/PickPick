96a7a8a42a3e81e84fbe720e90e15628d31ec096
minseong-dev <aa971020@gmail.com>
2023-01-02 15:01:25 +0900

    REPACTOR : entity 변수 명 변경으로 인한 함수명 변경

--- back/src/main/java/com/example/PickPick/repository/VideoLikeRepository.java
+++ back/src/main/java/com/example/PickPick/repository/VideoLikeRepository.java
@@ -14,7 +14,7 @@ public interface VideoLikeRepository extends JpaRepository<VideoLikeEntity, Inte
     @Query(value = "select count(*) from video_likes where video_likes.video_id=:videoId", nativeQuery = true)
     int countByVideoId(@Param("videoId") int videoId);
 
-    List<VideoLikeEntity> findByVideoId(VideoEntity video);
+    List<VideoLikeEntity> findByVideo(VideoEntity video);
 
     VideoLikeEntity findByUserIdAndVideoId(UserEntity user, VideoEntity video);
 
